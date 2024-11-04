25142ac8a7102884ece6de6ae8e69dfd5f063463
Casey Waldren <cwaldren@launchdarkly.com>
2024-11-04 10:31:39 -0800

    add InterpolateTemplate

--- pkgs/sdk/server-ai/src/LdAiConfigTracker.cs
+++ pkgs/sdk/server-ai/src/LdAiConfigTracker.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Mustache;
 
 namespace LaunchDarkly.Sdk.Server.Ai
 {
@@ -16,6 +18,17 @@ namespace LaunchDarkly.Sdk.Server.Ai
         public LdAiConfigTracker(LdClient client)
         {
             _client = client;
+        }
+
+        /// <summary>
+        /// Parses and interpolates a template string with the provided variables.
+        /// </summary>
+        /// <param name="template">the template string to be parsed and interpolated</param>
+        /// <param name="variables">a dictionary containing the variables to be used for interpolation</param>
+        /// <returns>the interpolated string</returns>
+        public string InterpolateTemplate(string template, IReadOnlyDictionary<string, object> variables)
+        {
+            return Template.Compile(template).Render(variables);
         }
 
         /// <summary>

3abb9e52547794597f613abdbdf055acf118b3f4
Casey Waldren <cwaldren@launchdarkly.com>
2024-11-06 15:51:34 -0800

    data model docs

--- pkgs/sdk/server-ai/src/DataModel/DataModel.cs
+++ pkgs/sdk/server-ai/src/DataModel/DataModel.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
-using LaunchDarkly.Sdk.Server.Ai;
-using LaunchDarkly.Sdk.Server.Ai.Config;
 
-#pragma warning disable CS1591
 namespace  LaunchDarkly.Sdk.Server.Ai.DataModel
 {
     /// <summary>
@@ -27,35 +22,69 @@ namespace  LaunchDarkly.Sdk.Server.Ai.DataModel
         Assistant
     }
 
+    /// <summary>
+    /// Represents the JSON serialization of the Meta field.
+    /// </summary>
     public class Meta
     {
+        /// <summary>
+        /// The version key.
+        /// </summary>
         [JsonPropertyName("versionKey")]
         public string VersionKey { get; set; }
+
+        /// <summary>
+        /// If the config is enabled.
+        /// </summary>
 
         [JsonPropertyName("enabled")]
         public bool Enabled { get; set; }
     }
+
+    /// <summary>
+    /// Represents the JSON serialization of a Message.
+    /// </summary>
     public class Message
     {
+        /// <summary>
+        /// The content.
+        /// </summary>
         [JsonPropertyName("content")]
         public string Content { get; set; }
 
+        /// <summary>
+        /// The role.
+        /// </summary>
         [JsonPropertyName("role")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public Role Role { get; set; }
     }
 
+
+    /// <summary>
+    /// Represents the JSON serialization of an AiConfig.
+    /// </summary>
+
     public class AiConfig
     {
+        /// <summary>
+        /// The prompt.
+        /// </summary>
         [JsonPropertyName("prompt")]
         public List<Message> Prompt { get; set; }
 
+        /// <summary>
+        /// LaunchDarkly metadata.
+        /// </summary>
+
         [JsonPropertyName("_ldMeta")]
         public Meta Meta { get; set; }
+
+        /// <summary>
+        /// The model params;
+        /// </summary>
 
         [JsonPropertyName("model")]
         public Dictionary<string, object> Model { get; set; }
     }
 }
-
-#pragma warning restore CS1591

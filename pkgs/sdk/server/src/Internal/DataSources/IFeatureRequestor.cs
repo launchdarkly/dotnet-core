d2ee70fcc41947be9de3b8d06808a551598ed619
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-03-03 16:42:52 -0800

    Progress

--- pkgs/sdk/server/src/Internal/DataSources/IFeatureRequestor.cs
+++ pkgs/sdk/server/src/Internal/DataSources/IFeatureRequestor.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using static LaunchDarkly.Sdk.Server.Subsystems.DataStoreTypes;
 
 namespace LaunchDarkly.Sdk.Server.Internal.DataSources
 {
+    internal class DataSetWithHeaders
+    {
+        public readonly FullDataSet<ItemDescriptor>? DataSet;
+        public readonly IEnumerable<KeyValuePair<string, IEnumerable<string>>> Headers;
+
+        public DataSetWithHeaders(FullDataSet<ItemDescriptor>? dataSet,
+            IEnumerable<KeyValuePair<string, IEnumerable<string>>> headers)
+        {
+            DataSet = dataSet;
+            Headers = headers;
+        }
+    }
+
     internal interface IFeatureRequestor : IDisposable
     {
-        Task<FullDataSet<ItemDescriptor>?> GetAllDataAsync();
+        Task<DataSetWithHeaders> GetAllDataAsync();
     }
 }

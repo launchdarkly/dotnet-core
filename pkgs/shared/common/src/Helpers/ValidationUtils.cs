dfa801e0f1ae4d0d7c9385d19653da3ab66906ed
jsonbailey <jbailey@launchdarkly.com>
2025-09-27 00:44:23 +0000

    undo common changes to prevent merge conflict

--- pkgs/shared/common/src/Helpers/ValidationUtils.cs
+++ pkgs/shared/common/src/Helpers/ValidationUtils.cs
@@ -9,39 +9,9 @@ namespace LaunchDarkly.Sdk.Helpers
     public static class ValidationUtils
     {
         private static readonly Regex ValidCharsRegex = new Regex("^[-a-zA-Z0-9._]+\\z");
-        private const int MaxSdkKeyLength = 8192;
 
         /// <summary>
-        /// Validates that a string does not contain invalid characters and is not too long for our systems.
-        /// </summary>
-        /// <param name="sdkKey">the SDK key to validate.</param>
-        /// <returns>Null if the input is valid, otherwise an error string describing the issue.</returns>
-        public static bool IsValidSdkKeyFormat(string sdkKey, out string errorMessage)
-        {
-
-            // For offline mode, we allow a null or empty SDK key and it is not invalid.
-            if (string.IsNullOrEmpty(sdkKey))
-            {
-                return true;
-            }
-
-            if (sdkKey.Length > MaxSdkKeyLength)
-            {
-                errorMessage = $"SDK key cannot be longer than {MaxSdkKeyLength} characters.";
-                return false;
-            }
-
-            if (!ValidCharsRegex.IsMatch(sdkKey))
-            {
-                errorMessage = "SDK key contains invalid characters.";
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Validates that a string is non-empty, not too long for our systems, and only contains
+        /// Validates that a string is non-empty, not too longer for our systems, and only contains
         /// alphanumeric characters, hyphens, periods, and underscores.
         /// </summary>
         /// <param name="s">the string to validate.</param>

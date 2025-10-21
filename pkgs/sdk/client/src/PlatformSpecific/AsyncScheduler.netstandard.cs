5a0c0cc93b6b6aae4a196b10ee7656ab787ab6bf
jsonbailey <jbailey@launchdarkly.com>
2025-10-21 17:39:20 -0500

    fix: Ensure PlatformScheduleAction order is preserved
    In Android/iOS these action are run on the main thread whereas in netstandard they are run in a threadpool and are not guaranteed to be run in order when we don't wait for the result.

--- pkgs/sdk/client/src/PlatformSpecific/AsyncScheduler.netstandard.cs
+++ pkgs/sdk/client/src/PlatformSpecific/AsyncScheduler.netstandard.cs
@@ -7,7 +7,15 @@ namespace LaunchDarkly.Sdk.Client.PlatformSpecific
     {
         private static void PlatformScheduleAction(Action a)
         {
-            Task.Run(a);
+            try
+            {
+                // Wait for the task to complete to ensure that actions are executed in the correct order.
+                Task.Run(a).Wait();
+            }
+            catch (Exception)
+            {
+                // Swallow exceptions to prevent them from propagating to the caller.
+            }
         }
     }
 }

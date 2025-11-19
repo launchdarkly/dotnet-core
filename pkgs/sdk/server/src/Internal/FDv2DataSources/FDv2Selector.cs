b688fe8be4b93e2c47a977b524c4e76db245c9b0
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-11-19 09:53:54 -0800

    Refactor error handling to be more granular.

--- pkgs/sdk/server/src/Internal/FDv2DataSources/FDv2Selector.cs
+++ pkgs/sdk/server/src/Internal/FDv2DataSources/FDv2Selector.cs
@@ -3,21 +3,21 @@ namespace LaunchDarkly.Sdk.Server.Internal.FDv2DataSources
     /// <summary>
     /// A selector can either be empty or it can contain state and a version.
     /// </summary>
-    internal struct Selector
+    internal struct FDv2Selector
     {
         public bool IsEmpty { get; }
         public int Version { get; }
         public string State { get; }
 
-        private Selector(int version, string state, bool isEmpty)
+        private FDv2Selector(int version, string state, bool isEmpty)
         {
             Version = version;
             State = state;
             IsEmpty = isEmpty;
         }
 
-        public static Selector Empty { get; } = new Selector(0, null, true);
+        public static FDv2Selector Empty { get; } = new FDv2Selector(0, null, true);
 
-        public static Selector Make(int version, string state) => new Selector(version, state, false);
+        public static FDv2Selector Make(int version, string state) => new FDv2Selector(version, state, false);
     }
 }

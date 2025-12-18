bba98480241b9011bc408d51db95245831890588
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-12-18 12:06:25 -0800

    Add synchronization to the initialization tracker.

--- pkgs/sdk/server/src/Internal/FDv2DataSources/FDv2DataSource.InitializationTracker.cs
+++ pkgs/sdk/server/src/Internal/FDv2DataSources/FDv2DataSource.InitializationTracker.cs
@@ -25,6 +25,7 @@ namespace LaunchDarkly.Sdk.Server.Internal.FDv2DataSources
         {
             private readonly TaskCompletionSource<bool> _taskCompletionSource = new TaskCompletionSource<bool>();
             private State _state = State.NoData;
+            private readonly object _stateLock = new object();
 
             private bool _initializersRemain;
             private bool _synchronizersRemain;
@@ -45,7 +46,7 @@ namespace LaunchDarkly.Sdk.Server.Internal.FDv2DataSources
                 /// The tracker has been informed that initializers are exhausted.
                 /// </summary>
                 InitializersExhausted,
-                
+
                 /// <summary>
                 /// State entered when we have been informed that we are falling back to FDv1.
                 /// </summary>
@@ -88,7 +89,7 @@ namespace LaunchDarkly.Sdk.Server.Internal.FDv2DataSources
                 /// We have received a selector.
                 /// </summary>
                 SelectorReceived,
-                
+
                 /// <summary>
                 /// We are attempting to fallback to FDv1.
                 /// </summary>
@@ -127,106 +128,109 @@ namespace LaunchDarkly.Sdk.Server.Internal.FDv2DataSources
 
             private void DetermineState(Action action)
             {
-                switch (_state)
+                lock (_stateLock)
                 {
-                    // Terminal states, ignore subsequent actions.
-                    case State.Initialized:
-                    case State.Failed:
-                        break;
-                    case State.NoData:
-                        switch (action)
-                        {
-                            case Action.DataReceived:
-                                _state = State.Data;
-                                break;
-                            case Action.InitializersExhausted:
-                                _initializersRemain = false;
-                                _state = State.InitializersExhausted;
-                                HandleRemainingSources();
-                                break;
-                            case Action.SelectorReceived:
-                                _state = State.Initialized;
-                                break;
-                            case Action.SynchronizersExhausted:
-                                _synchronizersRemain = false;
-                                HandleRemainingSources();
-                                break;
-                            case Action.FallbackSynchronizersExhausted:
-                                // This would indicate that we skipped going through the fallback
-                                // process. So this generally shouldn't be an achievable state.
-                                _state = State.Failed;
-                                break;
-                        }
+                    switch (_state)
+                    {
+                        // Terminal states, ignore subsequent actions.
+                        case State.Initialized:
+                        case State.Failed:
+                            break;
+                        case State.NoData:
+                            switch (action)
+                            {
+                                case Action.DataReceived:
+                                    _state = State.Data;
+                                    break;
+                                case Action.InitializersExhausted:
+                                    _initializersRemain = false;
+                                    _state = State.InitializersExhausted;
+                                    HandleRemainingSources();
+                                    break;
+                                case Action.SelectorReceived:
+                                    _state = State.Initialized;
+                                    break;
+                                case Action.SynchronizersExhausted:
+                                    _synchronizersRemain = false;
+                                    HandleRemainingSources();
+                                    break;
+                                case Action.FallbackSynchronizersExhausted:
+                                    // This would indicate that we skipped going through the fallback
+                                    // process. So this generally shouldn't be an achievable state.
+                                    _state = State.Failed;
+                                    break;
+                            }
 
-                        break;
-                    case State.Data:
-                        switch (action)
-                        {
-                            case Action.DataReceived:
-                                break;
-                            case Action.SynchronizersExhausted:
-                            case Action.FallbackSynchronizersExhausted:
-                            case Action.InitializersExhausted:
-                            case Action.SelectorReceived:
-                            case Action.FallingBack:
-                                _state = State.Initialized;
-                                break;
-                        }
+                            break;
+                        case State.Data:
+                            switch (action)
+                            {
+                                case Action.DataReceived:
+                                    break;
+                                case Action.SynchronizersExhausted:
+                                case Action.FallbackSynchronizersExhausted:
+                                case Action.InitializersExhausted:
+                                case Action.SelectorReceived:
+                                case Action.FallingBack:
+                                    _state = State.Initialized;
+                                    break;
+                            }
 
-                        break;
-                    case State.InitializersExhausted:
-                        switch (action)
-                        {
-                            case Action.InitializersExhausted:
-                                break;
-                            case Action.SynchronizersExhausted:
-                                _synchronizersRemain = false;
-                                HandleRemainingSources();
-                                break;
-                            case Action.FallbackSynchronizersExhausted:
-                                // This would indicate that we skipped going through the fallback
-                                // process. So this generally shouldn't be an achievable state.
-                                _state = State.Failed;
-                                break;
+                            break;
+                        case State.InitializersExhausted:
+                            switch (action)
+                            {
+                                case Action.InitializersExhausted:
+                                    break;
+                                case Action.SynchronizersExhausted:
+                                    _synchronizersRemain = false;
+                                    HandleRemainingSources();
+                                    break;
+                                case Action.FallbackSynchronizersExhausted:
+                                    // This would indicate that we skipped going through the fallback
+                                    // process. So this generally shouldn't be an achievable state.
+                                    _state = State.Failed;
+                                    break;
 
-                            case Action.FallingBack:
-                                _state = State.FallingBack;
-                                break;
-                            case Action.DataReceived:
-                            case Action.SelectorReceived:
-                                _state = State.Initialized;
-                                break;
-                        }
+                                case Action.FallingBack:
+                                    _state = State.FallingBack;
+                                    break;
+                                case Action.DataReceived:
+                                case Action.SelectorReceived:
+                                    _state = State.Initialized;
+                                    break;
+                            }
 
-                        break;
-                    case State.FallingBack:
-                        switch (action)
-                        {
-                            case Action.FallingBack:
-                            case Action.InitializersExhausted:
-                            case Action.SynchronizersExhausted:
-                                break;
-                            case Action.FallbackSynchronizersExhausted:
-                                _state = State.Failed;
-                                break;
-                            case Action.DataReceived:
-                            case Action.SelectorReceived:
-                                _state = State.Initialized;
-                                break;
-                        }
+                            break;
+                        case State.FallingBack:
+                            switch (action)
+                            {
+                                case Action.FallingBack:
+                                case Action.InitializersExhausted:
+                                case Action.SynchronizersExhausted:
+                                    break;
+                                case Action.FallbackSynchronizersExhausted:
+                                    _state = State.Failed;
+                                    break;
+                                case Action.DataReceived:
+                                case Action.SelectorReceived:
+                                    _state = State.Initialized;
+                                    break;
+                            }
 
-                        break;
-                }
+                            break;
+                    }
 
-                // After updating the state determine if we need to complete the task.
-                switch (_state)
-                {
-                    case State.Initialized:
-                        _taskCompletionSource.TrySetResult(true);
-                        break;
-                    case State.Failed:
-                        _taskCompletionSource.TrySetResult(false);
-                        break;
+                    // After updating the state determine if we need to complete the task.
+                    switch (_state)
+                    {
+                        case State.Initialized:
+                            _taskCompletionSource.TrySetResult(true);
+                            break;
+                        case State.Failed:
+                            _taskCompletionSource.TrySetResult(false);
+                            break;
+                    }
                 }
             }
 

543d1747b7fb32dd5586726586e593071c184ae5
Matthew M. Keeler <mkeeler@launchdarkly.com>
2026-05-12 10:42:29 -0400

    chore: remove FDv2 pre-release warnings for GA promotion (#257)

--- pkgs/sdk/server/src/Subsystems/ITransactionalDataStore.cs
+++ pkgs/sdk/server/src/Subsystems/ITransactionalDataStore.cs
@@ -17,10 +17,6 @@ namespace LaunchDarkly.Sdk.Server.Subsystems
     /// </remarks>
     /// <seealso cref="IPersistentDataStore"/>
     /// <seealso cref="IPersistentDataStoreAsync"/>
-    /// <para>
-    /// This interface is not stable, and not subject to any backwards compatibility guarantees or semantic versioning.
-    /// It is in early access. If you want access to this feature please join the EAP. https://launchdarkly.com/docs/sdk/features/data-saving-mode
-    /// </para>
     public interface ITransactionalDataStore
     {
         /// <summary>
--- pkgs/sdk/server/src/Subsystems/Selector.cs
+++ pkgs/sdk/server/src/Subsystems/Selector.cs
@@ -2,10 +2,6 @@ namespace LaunchDarkly.Sdk.Server.Subsystems
 {
     /// <summary>
     /// A selector can either be empty or it can contain state and a version.
-    /// <para>
-    /// This struct is not stable, and not subject to any backwards compatibility guarantees or semantic versioning.
-    /// It is in early access. If you want access to this feature please join the EAP. https://launchdarkly.com/docs/sdk/features/data-saving-mode
-    /// </para>
     /// </summary>
     public struct Selector
     {

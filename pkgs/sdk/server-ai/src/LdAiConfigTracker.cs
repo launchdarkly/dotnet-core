65ccd14f2c3888b2aae81fe58fda55e412b12295
Matt McCarthy <mmccarthy@launchdarkly.com>
2026-07-07 11:47:07 -0500

    fix: add missing System.Linq import to LdAiConfigTracker

--- pkgs/sdk/server-ai/src/LdAiConfigTracker.cs
+++ pkgs/sdk/server-ai/src/LdAiConfigTracker.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
